d0838a2880019e52a01012020c64603e8031b5c9
Andre Kho <andreyonathan65@gmail.com>
2022-03-26 14:34:02 +0700

    Add function for updating data and get single data

--- PHP IV/8-oopform/data.php
+++ PHP IV/8-oopform/data.php
@@ -86,8 +86,63 @@ function getSingleProduct($id)
     $conn = createConnection();
     $sql = "SELECT * FROM products WHERE id = $id";
     $result = $conn->query($sql, PDO::FETCH_ASSOC);
-    return $result->fetch();
+
+    $data = $result->fetch();
+
+    // Store each data in variables
+    $id = $data['id'];
+    $name = $data['name'];
+    $price = $data['price'];
+    $imageUrl = $data['image_url'];
+    $options = json_decode($data['options']); // Use json_decode() to decode JSON string into PHP object
+
+    // Create new objects based on their category value
+    switch ($options->category) {
+      case 'processor':
+        return new Processor($id, $name, $price, $imageUrl, $options->cores);
+        break;
+      case 'storage':
+        return new Storage($id, $name, $price, $imageUrl, $options->type);
+        break;
+      case 'memory':
+        return new RAM($id, $name, $price, $imageUrl, $options->size);
+        break;
+      case 'vga':
+        return new VGA($id, $name, $price, $imageUrl, $options->size);
+        break;
+    }
+    return;
   } catch (PDOException $e) {
     die('Error reading data: ' . $e->getMessage());
+  }
+}
+
+function updateProduct($payload)
+{
+  try {
+    $conn = createConnection();
+    $sql = 'UPDATE products SET name=?, price=?, image_url=?, options=? WHERE id=?';
+    $statement = $conn->prepare($sql);
+
+    $options = array(
+      'category' => $payload['category']
+    );
+
+    switch ($payload['category']) {
+      case 'processor':
+        $options['cores'] = $payload['cores'];
+        break;
+      case 'storage':
+        $options['type'] = $payload['storageType'];
+        break;
+      case 'memory':
+      case 'vga':
+        $options['size'] = $payload['size'];
+        break;
+    }
+
+    $statement->execute([$payload['name'], $payload['price'], $payload['imageUrl'], json_encode($options), $payload['id']]);
+  } catch (PDOException $e) {
+    die('Error creating data: ' . $e->getMessage());
   }
 }
